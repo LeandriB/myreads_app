46b41270fddb91dfece275c548d6550dec224dc3
LeandriB <leandrikuyk@gmail.com>
2020-11-28 20:49:18 -0500

    Pass static PropTypes to BookShelf component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -3,10 +3,14 @@ import PropTypes from 'prop-types'
 import Book from './Book'
 
 class BookShelf extends Component {
+    
+    static PropTypes = {
+        title: PropTypes.string.isRequired
+    }
     render() {
         return (
             <div className="bookshelf">
-                <h2 className="bookshelf-title">Currently Reading</h2>
+                <h2 className="bookshelf-title">{this.props.title}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
                         <Book/>
