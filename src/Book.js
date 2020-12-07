30603e63b365c3ab56bb88b68f1256bd84615d9f
LeandriB <leandrikuyk@gmail.com>
2020-12-07 11:58:44 -0500

    Pass static props to Book component

--- src/Book.js
+++ src/Book.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import ChangeShelf from './ChangeShelf.js'
 
 class Book extends Component {
+
+    static propTypes = {
+        book: PropTypes.object.isRequired
+    }
     render() {
-        const { book, bookshelf } = this.props
+        const { book } = this.props
         return(
             <li>
                 <div className="book">
                     <div className="book-top">
                         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
                         <ChangeShelf
-                            book={book} shelf={bookshelf}/>
+                            book={book}/>
                     </div>
                     <div className="book-title">To Kill a Mockingbird</div>
                     <div className="book-authors">Harper Lee</div>
