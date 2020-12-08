40289344d810ea21739cf6daf5ddea0610f45985
LeandriB <leandrikuyk@gmail.com>
2020-12-07 22:25:22 -0500

    Call handleChange method

--- src/ChangeShelf.js
+++ src/ChangeShelf.js
@@ -7,12 +7,18 @@ class ChangeShelf extends Component {
         book: PropTypes.object.isRequired,
     }
 
+    handleChange = (book , event) => {
+        event.preventDefault()
+        this.props.onMoveBook(book, event.target.value)
+        this.setState({shelf: event.target.value})
+    }    
+
     render() {
         const { book } = this.props
         const { shelf } = this.state
         return (
             <div className="book-shelf-changer">
-                <select value={shelf} >
+                <select value={shelf} onChange={(event) => this.handleChange(event, book)}>
                     <option value="move" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
