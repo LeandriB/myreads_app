19eaba4793f01de09a1e58aac5f90be15a27add2
LeandriB <leandrikuyk@gmail.com>
2020-12-13 22:37:44 -0500

    Filter through books on each shelf

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -10,25 +10,28 @@ class BookShelf extends Component {
         userBooks: PropTypes.array.isRequired,
         onMoveBook: PropTypes.func.isRequired
     }
-
+/*
     getBooks() {
-        const booksOnShelf = this.props.userBooks.filter((book) => book.shelf === this.props.id)
+        const { shelf } = this.props
+        const booksOnShelf = this.props.userBooks.filter((book) => book.shelf === shelf.id)
         return booksOnShelf
     }
-
+*/
     render() {
+        const { title, onMoveBook, userBooks } = this.props
+        const booksOnShelf = userBooks.filter(book => book.shelf === this.props.id)
+
         return (
             <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.title}</h2>
+                <h2 className="bookshelf-title">{title}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">  
-                        {this.getBooks().map((book) => (
+                        {booksOnShelf.map(book => (
                             <Book
                                 book={book}
                                 key={book.id}
-                                onMoveBook={(book, shelf) => {
-                                    this.props.onMoveBook(book, shelf)
-                                }}
+                                shelf={this.props.id}
+                                onMoveBook={onMoveBook}
                             />
                         ))}
                     </ol>
