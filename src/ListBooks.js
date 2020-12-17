3f1645caf7d0e8d22e54aa2747df22898777a9d6
LeandriB <leandrikuyk@gmail.com>
2020-12-16 21:17:50 -0500

    Refactor to functional component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,49 +1,44 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import BookShelf from './BookShelf'
 
-class ListBooks extends Component {
-    render() {
-        const bookshelves = [{
-            id: 'currentlyReading',
-            title: 'Currently Reading'
-        },
-        {
-            id: 'wantToRead',
-            title: 'Want to Read'
-        },
-        {
-            id: 'read',
-            title: 'Read'
-        }]
+const bookshelves = [{
+    id: 'currentlyReading',
+    title: 'Currently Reading'
+},
+{
+    id: 'wantToRead',
+    title: 'Want to Read'
+},
+{
+    id: 'read',
+    title: 'Read'
+}]
 
-        const { userBooks, onMoveBook} = this.props
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    {bookshelves.map((shelf, index) => {
-                        return <BookShelf 
-                            key={index}
-                            id={shelf.id}
-                            title={shelf.title}
-                            userBooks={userBooks}
-                            onMoveBook={onMoveBook}
-                        />
-                    })}
-                </div>
-                <div className="open-search">
-                    <Link 
-                        className="add-button"
-                        to='/search'>
-                            <button>Add a Book</button>
-                    </Link>
-                </div>
+const ListBooks = ({userBooks, onMoveBook}) => (
+    <div className="list-books">
+            <div className="list-books-title">
+            <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+                {bookshelves.map((shelf, index) => {
+                    return <BookShelf 
+                        key={index} // Todo: Refactor to use without index
+                        id={shelf.id}
+                        title={shelf.title}
+                        userBooks={userBooks}
+                        onMoveBook={onMoveBook}
+                    />
+                })}
+            </div>
+            <div className="open-search">
+                <Link 
+                    className="add-button"
+                    to='/search'>
+                        <button>Add a Book</button>
+                </Link>
+            </div>
         </div>
-        )
-    }
-}
+    )
 
 export default ListBooks
